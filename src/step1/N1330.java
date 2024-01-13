461b208825cf3f542a1f400c37fc911291d41953
gyeongmann <gyeongmann@github.com>
2024-01-14 00:25:14 +0900

    N1330

--- src/step1/N1330.java
+++ src/step1/N1330.java
@@ -1,7 +1,19 @@
 package step1;
 
+import java.util.Scanner;
+
 public class N1330 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
 
+        System.out.println(compare(A, B));
+    }
+
+    static String compare(int A, int B) {
+        if (A > B) return ">";
+        else if (A < B) return "<";
+        return "==";
     }
 }
