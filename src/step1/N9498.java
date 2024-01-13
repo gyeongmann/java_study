719fdda42485f59939a3026b2547a68bddd027a1
gyeongmann <hkc8857@naver.com>
2024-01-14 00:47:08 +0900

    N9498

--- src/step1/N9498.java
+++ src/step1/N9498.java
@@ -1,2 +1,20 @@
-package step1;public class N9498 {
+package step1;
+
+import java.util.Scanner;
+
+public class N9498 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int score = sc.nextInt();
+
+        System.out.println(calculateGrade(score));
+    }
+
+    static String calculateGrade(int score) {
+        if (score >= 90) return "A";
+        else if (score >= 80) return "B";
+        else if (score >= 70) return "C";
+        else if (score >= 60) return "D";
+        return "F";
+    }
 }
